1810c0dd90059016eec6d07b4361e10496d4f0ed
judasamok <39342882+judasamok@users.noreply.github.com>
2019-01-22 19:40:55 -0800

    New pages

---
+++ page-products.php
@@ -0,0 +1,31 @@
+<?php 
+    get_header();
+?>
+
+<section class="headerColorProducts">
+
+    <div class="container">
+      <div class="row justify-content-center">
+         <div class="col-lg-5 col-md-5 marginLogo">
+            <img class="img-fluid" src="<?php bloginfo('template_directory'); ?>/assets/img/logo.png" alt="Logo">
+         </div>
+      </div>
+    </div>
+
+    <div class="container headerMargin">
+        <div class="row justify-content-center">
+            <div class="col-4-lg col-4-md col-4-sm mt-4">
+                <h1 class="text-center">Productos</h1>                
+            </div>
+            <div class="">
+                <img class="img-fluid iconHeader" src="<?php bloginfo('template_directory'); ?>/assets/img/icon02-min.png" alt="Icon Contactos">
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<?php 
+    get_footer();
+?>
