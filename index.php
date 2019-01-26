919b20d0cba927f9b1dafb60d1d9a4b72658fc48
judasamok <39342882+judasamok@users.noreply.github.com>
2019-01-26 11:21:07 -0800

    New index

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
-    <?php get_header(); ?>  
+
+<?php get_header(); ?>  
+
      <!--Sección arriba-->
     <section class="image img-fluid">
-        <div class="container">
-      <div class="row justify-content-center">
-         <div class="col-lg-5 col-md-5 marginLogo">
-            <img class="img-fluid" src="<?php bloginfo('template_directory'); ?>/assets/img/logo.png" alt="Logo">
+      <div class="container">
+         <div class="row justify-content-center">
+            <div class="col-lg-5 col-md-5 marginLogo">
+               <img class="img-fluid" src="<?php bloginfo('template_directory'); ?>/assets/img/logo.png" alt="Logo">
+            </div>
          </div>
       </div>
-    </div>
     </section>
      
      <!--Nosotros-->
      <section>
          <div class="row no-gutters">
-             <div class="col-md-8 espacio">
+             <div class="col-md-8 col-sm-8 paddingText">
                  <h1 class="text-center">Nuestra<br>Historia</h1>
                     <h3 class="text-center">El principio de todo</h3>
                     <p class="text-center">Ut auctor luctus leo eleifend porttitor. Curabitur <br>pretit auctor luctus leo eleifend porttitor. Curabitur <br> pretium rutrum turpis, at varius lacus viverra ac. <br> Donec turpis magna, blandit eget sapien consectetur, <br> cursus interdum dolor. Pellentesque habitant morbi <br> tristique senectus et netus et malesuada fames ac <br> turpis egestas.</p>
@@ -29,15 +31,12 @@
      
      <!--Productos-->
      <section>
-         <div class="container-fliud">
-        <div class="col-12-lg col-12-md col-12-sm">
-            <img class="image img-fluid" src="<?php bloginfo('template_directory'); ?>/assets/img/img03-min.png" alt="Products">
-        </div>
-        <div class="col-md-8 up">
-            <h1 class="text-center">Nuestros<br>Productos</h1>
-            <h3 class="text-center">Deliciosa variedad de helados</h3>
-            <div class="col alignBtn">
-                <a class="buttons buttonProducts" href="./products">Ver más</a>
+         <div class="container-fluid bgImgHome textAlignImg">
+            <div class="col-md-8">
+               <h1 class="text-center">Nuestros<br>Productos</h1>
+               <h3 class="text-center">Deliciosa variedad de helados</h3>
+               <div class="col alignBtn">
+                  <a class="buttons buttonProducts" href="./products">Ver más</a>
             </div>
         </div>
     </div>
@@ -46,10 +45,10 @@
       <!--Sabores-->
       <section>
          <div class="row no-gutters">
-            <div class="col-md-8">
-                <img class="image img-fluid" src="<?php bloginfo('template_directory'); ?>/assets/img/img01-min.jpg" alt="Flavors">
+            <div class="col-lg-7 col-md-7 col-sm-7">
+                <img class="img-fluid" src="<?php bloginfo('template_directory'); ?>/assets/img/img01-min.jpg" alt="Flavors">
             </div>
-            <div class="col-md-4 espacio">
+            <div class="col-md-4 padding">
                 <h1 class="text-center">Sabores</h1>
                 <h3 class="text-center">Conoce todos nuestros sabores</h3>
                 <p class="text-center">Ut auctor luctus leo eleifend porttitor. <br> Curabitur pretit auctor luctus leo <br> eleifend porttitor. Curabitur pretium <br> rutrum turpis, at varius lacus viverra ac. <br> Donec turpis magna, blandit eget sapien <br> consectetur, cursus interdum dolor.</p>
@@ -63,7 +62,7 @@
       <!--Promociones-->
       <section>
           <div class="row no-gutters">
-             <div class="col-md-8 espacio">
+             <div class="col-md-8 paddingText">
                  <h1 class="text-center">Promociones</h1>
                     <h3 class="text-center">Enterate de nuestros descuentos</h3>
                     <p class="text-center">Suscríbete a nuestro newsletter para <br> obtener más información acerca de <br> nuestras promociones.</p>
@@ -90,4 +89,3 @@
       </section>
     
     <?php get_footer(); ?>
-
