91fdc5e59acf8b1c227b4eba5860d895509e4206
kerdasilvab <kerdasilvab@gmail.com>
2019-01-30 01:21:07 -0600

    Error 404

---
+++ 404.php
@@ -0,0 +1,25 @@
+    <?php get_header(); ?>
+    
+    <section class="imageError img-fluid">
+       </section>
+       <section class=" container">
+        <div class="row">
+            <div class="row col-xs-12 col-sm-12 col-md-6 col-lg-6 ">
+             
+               <div class=" col-xs-12 col-sm-12 col-md-8 col-lg-8 ">
+                   <h4 class="text-center">Error <br> 404</h4>
+                <p1 class="text-center">Página no encontrada</p1>
+                <div class="col alignBtn">
+                    <a class="buttons " href="./about-us">Regresar</a>
+                </div>
+               </div>
+                
+             </div>
+             
+             <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6  ">
+                 <img class="img-fluid" src="<?php bloginfo('template_directory'); ?>/assets/img/img33-min.png" alt="About Us">
+             </div>
+        </div>
+    </section>
+    <br>
+   <?php get_footer(); ?>
