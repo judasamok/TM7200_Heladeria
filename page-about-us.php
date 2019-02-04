e13ab0856b1377f729ed970ed5a9a52328d7169d
kerdasilvab <kerdasilvab@gmail.com>
2019-02-03 23:19:36 -0600

    Actualización del about us

--- page-about-us.php
+++ page-about-us.php
@@ -1,16 +1,56 @@
-<?php 
-    get_header();
-?>
+<?php get_header(); ?>
 
-<?php
-    if(have_posts()):
-        while(have_posts()) : the_post(); ?>
-            <p><?php the_content(); ?></p>
-        <?php endwhile;
-    else :
-        echo "<p>No hay contenido.</p>";
-    endif;
-?>
-<?php 
-    get_footer();
-?>
+<section class="headerColorAboutUs">
+   <div class="container">
+      <div class="row justify-content-center">
+         <div class="col-lg-5 col-md-5 marginLogo">
+            <img class="img-fluid" src="<?php bloginfo('template_directory'); ?>/assets/img/logo.png" alt="Logo">
+         </div>
+      </div>
+    </div>
+    <div class="container headerMargin">
+        <div class="row justify-content-center">
+            <div class="col-4-lg col-4-md col-4-sm mt-4">
+                <h1 class="text-center">Acerca de nosotros</h1>
+            </div>
+            <div class="">
+                <img class="img-fluid iconHeader" src="<?php bloginfo('template_directory'); ?>/assets/img/icon01-min.png" alt="Icon Nosotros">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+   <br>
+    <div class="container">
+       <div class="row justify-content-center">
+           <div class="col-xs-12 col-sm-12 col-md-8 col-lg-7">
+            <h1 class="text-center">Nuestra<br>Historia</h1>
+            <h3 class="text-center">El principio de todo</h3> 
+            <p class="text-center">Ut auctor luctus leo eleifend porttitor. Curabitur pretit auctor luctus leo eleifend porttitor. Curabitur pretium rutrum turpis, at varius lacus viverra ac. Donec turpis magna, blandit eget sapien consectetur, cursus interdum dolor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut auctor luctus leo eleifend porttitor. Curabitur pretit auctor luctus leo eleifend porttitor. Curabitur pretium rutrum turpis, at varius lacus viverra ac. Donec turpis magna, blandit eget sapien consectetur, cursus interdum dolor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div>
+        <img class="img-fluid" src="<?php bloginfo('template_directory'); ?>/assets/img/img06-min.png" alt="Img Nosotros">
+    </div>
+</section>
+
+<section>
+   <br>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xs-12 col-sm-12 col-md-8 col-lg-7">
+             <h1 class="text-center">Helados hechos con amor</h1>
+             <h3 class="text-center">Lo que nos importa es que los clientes sean felices</h3> 
+             <p class="text-center">Ut auctor luctus leo eleifend porttitor. Curabitur pretit auctor luctus leo eleifend porttitor. Curabitur pretium rutrum turpis, at varius lacus viverra ac. Donec turpis magna, blandit eget sapien consectetur, cursus interdum dolor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut auctor luctus leo eleifend porttitor. Curabitur pretit auctor luctus leo eleifend porttitor. Curabitur pretium rutrum turpis, at varius lacus viverra ac. Donec turpis magna, blandit eget sapien consectetur, cursus interdum dolor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php get_footer(); ?>
